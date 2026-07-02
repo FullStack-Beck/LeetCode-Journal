a273adefe8e36d56ea8c0ac6620768600c6e9548
Gabriel <gabrielbeck.work@gmail.com>
2026-07-02 08:35:11 -0500

    Clarify palindrome solution and add new implementation
    Updated comments to clarify solution limitations and added a new implementation that does not convert the number to a string.

--- Easy/0009-palindrome-number.py
+++ Easy/0009-palindrome-number.py
@@ -11,7 +11,7 @@ Space Complexity: O(n) - to store the string representation.
 """
 
 """
-This is what I created
+This is what I created, this solution only works with numbers under 1000 see new solution at the very bottom
 """
 class Solution:
     def isPalindrome(self, x: int) -> bool:
@@ -35,7 +35,7 @@ class Solution:
         else: return False
 
 """
-this is an easier solution
+this is an easier solution, works by converting to a string
 """
 class SolutionB:
     def isPalindromeB(self, x: int) -> bool:
@@ -50,3 +50,28 @@ if __name__ == "__main__":
     sol = Solution()
     print(sol.isPalindrome(121))   # Expected: True
     print(sol.isPalindrome(-121))  # Expected: False
+
+"""
+this version doesnt convert to a string
+"""
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # if x is less then 0 or if the remainder = 0 and x doesnt = 0 return false
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        # save the original number so we can compare
+        original = x
+        reverse = 0 # a placeholder value before we flip the number around
+
+        while x > 0:
+            digit = x % 10 # 121 = 12.1 the % keeps the .1
+            reverse = (reverse * 10) + digit # reverse = 0 * 10 + 1 reverse is now 1
+            print(reverse)
+            print(digit)
+            x = x // 10 # 121 is now 12 repeat
+            print(x)
+        # second run 12 % 10 is now 1.2 keep the 2
+        # reverse = 1 * 10 + 2 reverse is now 12
+        
+        return original == reverse
